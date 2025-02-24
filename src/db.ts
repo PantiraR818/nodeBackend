a2091bc273d2c58492304d7368635ec6cf62692e
unknown <pantiraploy818@gmail.com>
2025-02-24 16:16:42 +0700

    finnish

--- src/db.ts
+++ src/db.ts
@@ -1,18 +1,18 @@
 import { Sequelize } from "sequelize-typescript";
 
-// const seq = new Sequelize('db_wellbeing', 'root', '', {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     models: [__dirname + '/models'],
-//     logging: console.log, 
-// });
-
-const seq = new Sequelize('db_wellbeing', 'postgres', '1234', {
+const seq = new Sequelize('db_wellbeing', 'root', '', {
     host: 'localhost',
-    dialect: 'postgres',
+    dialect: 'mysql',
     models: [__dirname + '/models'],
     logging: console.log, 
 });
+
+// const seq = new Sequelize('db_wellbeing', 'postgres', '1234', {
+//     host: 'localhost',
+//     dialect: 'postgres',
+//     models: [__dirname + '/models'],
+//     logging: console.log, 
+// });
 
 
 export default seq;
